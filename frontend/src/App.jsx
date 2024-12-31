674ffa440144a5fedc166ebabff6aabf7fb99d91
Andre Silva <andrenations@gmail.com>
2024-12-31 04:15:48 -0500

    Integrated GameGrid component into the main App component. Created a responsive and centered layout using Tailwind CSS for better user experience. Added a gradient background container for visual appeal. Included a title, "Match 2 Game," with styled typography to introduce the game.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import GameGrid from "./components/GameGrid"
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="flex justify-center items-center h-screen bg-gray-950">
+      <div className="p-5 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 shadow-lg rounded-md">
+        <h1 className="text-2xl text-white font-[10px] text-center mb-4">Match 2 Game</h1>
+        <GameGrid />
+      </div>
+    </div>
   )
 }
 
